fe8df9d30f4ac1f0cb4b73729899e71706a5d605
DimonDemchenko <sl760674@gmail.com>
2022-12-06 15:25:41 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <button onClick={Req}>Знайти пиво</button>
-    </div>
-  );
-}
+  const API = "ed2eb291a45f1ecaf471c5d34fe79cbf";
+  // https://www.thecocktaildb.com/api.php
+  // const res = fetch(
+  //   `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${API}`
+  // )
+  //   .then((response) => response.json())
+  //   .then((response) => {
+  //     console.log(response);
+  //   });
 
+  const gettindWeather = async () => {
+    const api_url = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${API}`
+    );
+    var data = await api_url.json();
+  };
+
+  return <p>{data}</p>;
+
+}
 export default App;
+
